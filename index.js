c86e09e98092e3c630a1440537641a0c2491fbf7
Reynold254 <reynold.burudi@student.moringaschool.com>
2025-03-18 14:41:50 +0200

    done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+// Declare customerName in global scope
+var customerName = "bob";
+
+// Function to uppercase customerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Function to set bestCustomer in global scope
+function setBestCustomer() {
+  bestCustomer = "not bob"; // Implicit global variable (bad practice, but required for the lab)
+}
+
+// Function to overwrite bestCustomer
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+// Declare leastFavoriteCustomer as a constant
+const leastFavoriteCustomer = "someone";
+
+// Function to attempt changing leastFavoriteCustomer (this will cause an error)
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "another person"; // This will throw an error since constants cannot be reassigned
+}
